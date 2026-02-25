cef76e7d25a74c272e557786a54d81ca22b567a5
César Alejandro Tolentino Mendoza <129608820+Tole15@users.noreply.github.com>
2026-02-25 06:01:23 -0600

    Actualizar wdt_100ms.vhd

--- fpga/v3v2/wdt_100ms.vhd
+++ fpga/v3v2/wdt_100ms.vhd
@@ -2,13 +2,12 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
--- 48 MHz / 100 ms = 4 800 000 cuentas ? 23 bits bastan (2²³-1 = 8 388 607)
 entity wdt_100ms is
   port(
     clk      : in  std_logic;
     rst      : in  std_logic;
-    kick     : in  std_logic;              -- pulso: llegó DUTY (load_pwm)
-    wdt_ok   : out std_logic               -- '1' = dentro de plazo
+    kick     : in  std_logic;              
+    wdt_ok   : out std_logic               
   );
 end entity;
 
@@ -21,7 +20,7 @@ begin
     if rising_edge(clk) then
       if rst='1' then
         cnt <= (others=>'0');
-      elsif kick='1' then                 -- reinicio al llegar pareja nueva
+      elsif kick='1' then                 
         cnt <= (others=>'0');
       elsif cnt < TOP then
         cnt <= cnt + 1;
