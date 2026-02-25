0d282be0fc21de8e181ad632b3405f14540f1485
César Alejandro Tolentino Mendoza <129608820+Tole15@users.noreply.github.com>
2026-02-25 05:56:07 -0600

    Actualizar tb_pwm.vhd

--- fpga/v3v2/tb_pwm.vhd
+++ fpga/v3v2/tb_pwm.vhd
@@ -35,12 +35,12 @@ begin
     wait for 200 ns;      -- Reset pulse
     rst <= '0';
 
-    ancho <= "0111";      -- 7/16 ˜ 43.75 %
+    ancho <= "0111";      -- 7/16 Ëœ 43.75 %
     load  <= '1';
     wait for CLK_PER;
     load  <= '0';
 
-    wait for 2 ms;        -- Observe varios ciclos (40 periodos)
-    assert false report "FIN de simulación" severity failure;
+    wait for 2 ms;        
+    assert false report "FIN de simulaciÃ³n" severity failure;
   end process;
 end sim;
